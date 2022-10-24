387d4cff77409c62dbcb6594390572b0b693b063
jbrekhus@gmail.com <jbrekhus@gmail.com>
2022-10-24 13:48:15 -0500

    fixed

--- install-all.sh
+++ install-all.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+SECONDS=0
 bash ./install-nagios.sh
 bash ./install-pnp4nagios.sh
 bash ./install-nrpe.sh
 bash ./install-highchart.sh
 
 
-sudo systemctl restart apache2.service
-sudo systemctl restart nagios.service
+
 sudo systemctl restart nrpe.service
 sudo systemctl restart npcd.service
+sudo systemctl restart apache2.service
+sudo systemctl restart nagios.service
 
+echo "Elapsed Time (using \$SECONDS): $SECONDS seconds"
