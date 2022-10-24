b67e6ef4f625d401013062beff47bb56ac4c4af6
jbrekhus@gmail.com <jbrekhus@gmail.com>
2022-10-24 13:31:34 -0500

    fixed

--- install-all.sh
+++ install-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 bash ./install-nagios.sh
-bash ./install-nrpe.sh
 bash ./install-pnp4nagios.sh
+bash ./install-nrpe.sh
 bash ./install-highchart.sh
+
+
+sudo systemctl restart apache2.service
+sudo systemctl restart nagios.service
+sudo systemctl restart nrpe.service
+sudo systemctl restart npcd.service
 
--- install-pnp4nagios-old.sh
+++ install-pnp4nagios-old.sh
@@ -1,39 +1,50 @@
+#!/bin/bash
 #installing PNP4Nagios Graphing
 SECONDS=0
-udo apt-get update
-echo "More stuff to make work on ub22"
-sudo apt-get install -y software-properties-common ca-certificates lsb-release apt-transport-https
-#add new repositry to isntall php7
+echo "$SECONDS seconds: adding ppa:ondrej/php repository to install php7.4" | tee -a ~/log.txt
 sudo yes | LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 sudo apt-get update
-echo "INSTALL NAGIOS APT"
-sudo apt-get install -y autoconf gcc libc6 make wget unzip apache2 php7.4 libapache2-mod-php7.4 libgd-dev
-echo "INSTALL SSL required 4.4.7"
-sudo apt-get install -y openssl libssl-dev
-echo "INSTALL NAGIOS PLUGINS APT"
-sudo apt-get install -y autoconf gcc libc6 libmcrypt-dev make libssl-dev wget bc gawk dc build-essential snmp libnet-snmp-perl gettext
-echo "INSTALL PNP4NAGIOS"
-sudo apt-get install -y rrdtool librrds-perl php7.4-gd php7.4-xml rdtool   
-
+sudo apt-get install -y rrdtool librrds-perl php7.4-gd php7.4-xml rdtool    
+echo "$SECONDS seconds: apt install completed" | tee -a ~/log.txt
+echo "$SECONDS seconds: Downloading pnp4nagios.tar.gz" | tee -a ~/log.txt
 cd /tmp
+
+i=1
+while [ $i -ge 1 ]
+do
 wget -O pnp4nagios.tar.gz https://github.com/lingej/pnp4nagios/archive/0.6.26.tar.gz
+#need a check to see if unable to extract
+if [ $? = 0 ]; then
+echo "$SECONDS seconds: succesfully download pnp4nagios.tar.gz"  | tee -a ~/log.txt
+i=0
+else
+    echo "ERROR download failed pnp4nagios.tar.gz"  | tee -a ~/log.txt
+    sleep 5
+    echo "Trying download again pnp4nagios.tar.gz"  | tee -a ~/log.txt
+    rm pnp4nagios.tar.gz
+    ((i++))    
+fi
+done
+
+
+
 tar xzf pnp4nagios.tar.gz
 cd /tmp/pnp4nagios-0.6.26
 sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled
+echo "$SECONDS seconds: making pnp4nagios"  | tee -a ~/log.txt
 sudo make all
 sudo make install
 sudo make install-webconf
 sudo make install-config
 sudo make install-init
+
+echo "$SECONDS seconds: starting npcd.service pnp4nagios"  | tee -a ~/log.txt
 sudo systemctl daemon-reload
 sudo systemctl enable npcd.service
-echo " ##############################"
-echo " ##  npcd started            ##"
-echo " ##############################"
 sudo systemctl start npcd.service
-sudo systemctl restart apache2.service
 
 #settting up nagios for pnp
+echo "$SECONDS seconds: editing nagios.cfg"  | tee -a ~/log.txt
 sudo sh -c "sed -i 's/process_performance_data=0/process_performance_data=1/g' /usr/local/nagios/etc/nagios.cfg"
 sudo sh -c "sed -i 's/#host_perfdata_file=/host_perfdata_file=/g' /usr/local/nagios/etc/nagios.cfg"
 sudo sh -c "sed -i 's/^host_perfdata_file=.*/host_perfdata_file=\/usr\/local\/pnp4nagios\/var\/service-perfdata/g' /usr/local/nagios/etc/nagios.cfg"
@@ -48,6 +59,7 @@ sudo sh -c "sed -i 's/#service_perfdata_file_mode=/service_perfdata_file_mode=/g
 sudo sh -c "sed -i 's/^#service_perfdata_file_processing_interval=.*/service_perfdata_file_processing_interval=15/g' /usr/local/nagios/etc/nagios.cfg"
 sudo sh -c "sed -i 's/^#service_perfdata_file_processing_command=.*/service_perfdata_file_processing_command=process-service-perfdata-file-bulk-npcd/g' /usr/local/nagios/etc/nagios.cfg"
 
+echo "$SECONDS seconds: editing command.cfg"  | tee -a ~/log.txt
 sudo sh -c "echo '' >> /usr/local/nagios/etc/objects/commands.cfg"
 sudo sh -c "echo 'define command {' >> /usr/local/nagios/etc/objects/commands.cfg"
 sudo sh -c "echo '    command_name    process-host-perfdata-file-bulk-npcd' >> /usr/local/nagios/etc/objects/commands.cfg"
@@ -60,7 +72,7 @@ sudo sh -c "echo '    command_line    /bin/mv /usr/local/pnp4nagios/var/service-
 sudo sh -c "echo '    }' >> /usr/local/nagios/etc/objects/commands.cfg"
 sudo sh -c "echo '' >> /usr/local/nagios/etc/objects/commands.cfg"
 
-
+echo "$SECONDS seconds: editing templates.cfg"  | tee -a ~/log.txt
 sudo sh -c "echo '' >> /usr/local/nagios/etc/objects/templates.cfg"
 sudo sh -c "echo 'define host {' >> /usr/local/nagios/etc/objects/templates.cfg"
 sudo sh -c "echo '   name       host-pnp' >> /usr/local/nagios/etc/objects/templates.cfg"
@@ -75,16 +87,20 @@ sudo sh -c "echo '   register   0' >> /usr/local/nagios/etc/objects/templates.cf
 sudo sh -c "echo '}' >> /usr/local/nagios/etc/objects/templates.cfg"
 sudo sh -c "echo '' >> /usr/local/nagios/etc/objects/templates.cfg"
 
-
 sudo sh -c "sed -i '/name.*generic-host/a\        use                             host-pnp' /usr/local/nagios/etc/objects/templates.cfg"
 sudo sh -c "sed -i '/name.*generic-service/a\        use                             service-pnp' /usr/local/nagios/etc/objects/templates.cfg"
 
 sudo rm -f /usr/local/pnp4nagios/share/install.php
-echo "Fixing PNP4 code"
+
+echo "Fixing PNP4 code  should use sed" | tee -a ~/log.txt
 sudo wget -O /usr/local/pnp4nagios/lib/kohana/system/libraries/Input.php https://raw.githubusercontent.com/jbrek/nagios4.4.7/main/Input.php
 sudo wget -O /usr/local/pnp4nagios/share/application/models/data.php https://raw.githubusercontent.com/jbrek/nagios4.4.7/main/data.php
 
-echo "log file at /tmp/install-pnp4nagios.log" | tee -a ~/log.txt
+echo "Restarting services " | tee -a ~/log.txt
+sudo systemctl start npcd.service
+sudo systemctl restart apache2.service
+sudo systemctl restart nagios.service
+echo "log file at /tmp/install-pnp4nagios.log"
 echo "Elapsed Time (using \$SECONDS): $SECONDS seconds" | tee -a ~/log.txt
 cat ~/log.txt
 cp ~/log.txt /tmp/install-pnp4nagios.log
